e7b6a0719483e06124d7892b8e62246991279e40
pero <G00422221@atu.ie>
2024-10-09 15:28:52 +0100

    part 2 complete

--- src/main/java/ie/atu/week3lab3/Person.java
+++ src/main/java/ie/atu/week3lab3/Person.java
@@ -15,15 +15,13 @@ public class Person {
     @NotBlank(message = "Must Enter Title")
     String title;
 
-    @NotBlank(message = "Must enter employee id")
+    //@NotBlank(message = "Must enter employee id")
     @PositiveOrZero(message = "employee id cannot be negative")
     int employeeId;
 
-    @NotBlank(message = "Must Enter age")
     @PositiveOrZero(message = "age must be 0 or greater")
     int age;
 
-    @NotBlank(message = "Must enter email")
     @Email(message = "Not a valid email")
     String email;
 
